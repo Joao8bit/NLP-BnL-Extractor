878ce0c35b7118f25e0be3b4907cc5f678b80dcc
Joao8bit <jpteigao@protonmail.com>
2022-10-03 21:46:58 +0200

    fixed name in function

--- adExtractor.py
+++ adExtractor.py
@@ -4,10 +4,10 @@ import subprocess
 def initial_setup():
     response = input('Is this your first time running this program? (Y/y, N/n)\n').lower()
     while response not in ("y", "n"):
-        response = input("Is this your first time running this program? (Y/y, N/n)\n").lower()
-    if(response == 'Y' or response == 'y'):
-        subprocess.run(['bash', 'startHere.sh'])
-    elif(response == 'N' or response == 'n'):
+        response = input("Is this your first time running this program? (Y/y, N/n) ").lower()
+    if(response == 'y'):
+        subprocess.run(['bash', 'setup.sh'])
+    elif(response == 'n'):
         pass
     
 def main():
