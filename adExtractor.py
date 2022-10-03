78a03def9392d3706847e4b6059559699101e49a
Joao8bit <jpteigao@protonmail.com>
2022-10-03 20:56:09 +0200

    More code cleaning, setup renaming

--- adExtractor.py
+++ adExtractor.py
@@ -1,14 +1,18 @@
 import os 
 import subprocess
 
-def main():        
+#Checks and runs the setup.sh file for the first time
+def initial_setup():
     response = input('Is this your first time running this program? (Y/y, N/n)\n').lower()
     while response not in ("y", "n"):
-        response = input("Is this your first time running this program? (Y/y, N/n)\n")
+        response = input("Is this your first time running this program? (Y/y, N/n)\n").lower()
     if(response == 'Y' or response == 'y'):
         subprocess.run(['bash', 'startHere.sh'])
     elif(response == 'N' or response == 'n'):
         pass
+    
+def main():
+    initial_setup()
 
 if __name__=="__main__":
     main()
