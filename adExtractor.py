07bc844df7aa068f8761d437890cf26dd884b15f
Joao8bit <jpteigao@protonmail.com>
2022-10-03 15:12:02 +0200

    Cleaned Python code, added separate initial sh script

--- adExtractor.py
+++ adExtractor.py
@@ -1,31 +1,12 @@
 import os 
-
-commands={
-        "download": "wget -P . https://data.bnl.lu/open-data/digitization/newspapers/export01-newspapers1841-1878.zip",
-        "unzip": 'unzip -q export01-newspapers1841-1878.zip',
-        "remove zip": "rm export01-newspapers1841-1878.zip",
-        "mkdir ADS": "mkdir ADS",
-        "move AD files": "find . -name '*ADVERTISEMENT*.xml' -exec cp {} ./ADS/ \;",
-        }
-
-def prepare_package():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print('Downloading dataset pack...\n')
-    os.system(commands['download'])
-    print('\nDownload complete! Unzipping file and deleting zip...')
-    os.system(commands['unzip'])
-    os.system(commands['remove zip'])
-    print('\nUnzipping complete! Moving all ADVERTISEMENT files to ADS folder...')
-    os.system(commands['mkdir ADS'])
-    os.system(commands['move AD files'])
-    print('\nOperation complete, you are ready to start!')
+import subprocess
 
 def main():        
-    response = input('Is this your first time running this program? (Y/y, N/n)\n')
-    while response not in ("Y", "N", "y", "n"):
+    response = input('Is this your first time running this program? (Y/y, N/n)\n').lower()
+    while response not in ("y", "n"):
         response = input("Is this your first time running this program? (Y/y, N/n)\n")
     if(response == 'Y' or response == 'y'):
-        prepare_package()
+        subprocess.run(['bash', 'startHere.sh'])
     elif(response == 'N' or response == 'n'):
         pass
 
---
+++ startHere.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+case "$OSTYPE" in	#Check OS
+
+  darwin*)  clear ;; 	#OSX
+  linux*)   clear ;;	#Linux
+  msys*)    cls ;;	#Windows
+  cygwin*)  cls ;;	#Windows
+
+esac
+
+echo -e $"Downloading dataset pack...\n"
+
+#Download dataset from source
+wget -P . https://data.bnl.lu/open-data/digitization/newspapers/export01-newspapers1841-1878.zip
+
+#Extracting zip and removing zip
+echo -e $"\nDownload complete! Unzipping file and deleting zip..." 
+unzip -q export01-newspapers1841-1878.zip
+rm export01-newspapers1841-1878.zip
+echo -e $"Unzipping complete! Moving all ADVERTISEMENT files to ADS folder...(can take up to five minutes)"
+
+#Moving all ADVERTISEMENT files to ADS/
+mkdir ADS
+find . -name '*ADVERTISEMENT*.xml' -exec cp {} ./ADS/ \;
+
+echo -e $"Operation complete, you are ready to start!"
