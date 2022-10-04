517000a038fb0bccb3261d14fe8cc314900cd302
JoaoP <jpteigao@protonmail.com>
2022-10-04 15:45:46 +0200

    Added support for the remaining dc: tags, cleaned code a bit

--- adExtractor.py
+++ adExtractor.py
@@ -5,7 +5,14 @@ from xml.etree import cElementTree as ET
 #We want to keep track of the different tags that might interest us
 #here from the XML files structure
 tag_dict={
+    "identifier": "{http://purl.org/dc/elements/1.1/}identifier",
+    "source": "{http://purl.org/dc/elements/1.1/}source",
+    "date": "{http://purl.org/dc/elements/1.1/}date",
+    "publisher": "{http://purl.org/dc/elements/1.1/}publisher",
+    "relation": "{http://purl.org/dc/elements/1.1/}relation",
     "description": "{http://purl.org/dc/elements/1.1/}description",
+    "title": "{http://purl.org/dc/elements/1.1/}title",
+    "type": "{http://purl.org/dc/elements/1.1/}type"
 }
 #Checks and runs the setup.sh file for the first time if the folder ./ADS/ does not yet exist
 def initial_setup():
@@ -28,9 +35,14 @@ def extract_tag(tag_name):
     root = tree.getroot()
 
     #Print data from the specified tag
-    for node in tree.iter(tag_name):
-        for elem in node.iter():
-            print(elem.text)
+    for tag in tag_dict:
+        for node in tree.iter():
+            print('\n')
+            for elem in node.iter():
+                print("{}: {}".format(format_tag(elem), elem.text))
+
+def format_tag(elem):
+    return elem.tag.replace("{http://purl.org/dc/elements/1.1/}","")
 
 def main():
     initial_setup()
