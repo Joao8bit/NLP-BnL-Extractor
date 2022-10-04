7afd3e9e0a6e220c3fa033f91b5b919872d4be36
JoaoP <jpteigao@protonmail.com>
2022-10-04 15:28:47 +0200

    Extraction of XML tags available now

--- adExtractor.py
+++ adExtractor.py
@@ -1,17 +1,41 @@
+import os
 import subprocess
+from xml.etree import cElementTree as ET
 
-#Checks and runs the setup.sh file for the first time
+#We want to keep track of the different tags that might interest us
+#here from the XML files structure
+tag_dict={
+    "description": "{http://purl.org/dc/elements/1.1/}description",
+}
+#Checks and runs the setup.sh file for the first time if the folder ./ADS/ does not yet exist
 def initial_setup():
-    response = input('Is this your first time running this program? (Y/y, N/n)\n').lower()
-    while response not in ("y", "n"):
-        response = input("Is this your first time running this program? (Y/y, N/n) ").lower()
-    if(response == 'y'):
+    isExist = os.path.exists('./ADS/')
+    if(isExist == False):
         subprocess.run(['bash', 'setup.sh'])
-    elif(response == 'n'):
+    else:
         pass
+
+def extract_tag(tag_name):
+    """
+    1. Read the XML file
+    2. Get the namespace for specific tags
+    3. Iter the needed tag name
+    4. Print the content of that tag
+    """
     
+    #Read contents from XML file
+    tree = ET.parse('257048-ADVERTISEMENT-DTL303.xml')
+    root = tree.getroot()
+
+    #Print data from the specified tag
+    for node in tree.iter(tag_name):
+        for elem in node.iter():
+            print(elem.text)
+
 def main():
     initial_setup()
+    extract_tag(tag_dict["description"])
 
 if __name__=="__main__":
     main()
+
