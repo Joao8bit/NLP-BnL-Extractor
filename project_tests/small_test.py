e369e78d93166a42f0ffe2ee79af0f6c7b44b0d0
Joao8bit <jpteigao@protonmail.com>
2022-10-06 15:36:30 +0200

    Main will run first file for test, cleaned code

--- project_tests/small_test.py
+++ project_tests/small_test.py
@@ -1,13 +1,13 @@
+import os
 from src.xml.xml_manipulation import *
 from src.nlp.nlp import *
-import subprocess
 
 def description_extractor():    
-    subprocess.run(['cp', './ADS/257048-ADVERTISEMENT-DTL303.xml','./'])    #Copy a single file to work with
-    tree = ET.parse('257048-ADVERTISEMENT-DTL303.xml')                      #Initialization of an XMl file
+    root_dir = './ADS/'
+    file = os.listdir(root_dir)                         #Gets a list of files in root_dir
+    tree = ET.parse(f"{root_dir}/{file[0]}")            #Initialization of the first XMl file
     print('\nPrinting description...\n')
-    data = extract_tag(tree, tag_dict["description"])                       #Extract description tag data
-    print(data)
-    print(get_lang(data))     
-    print_lang_count()                                           #Language displayer as a dictionary
-    subprocess.run(['rm', '257048-ADVERTISEMENT-DTL303.xml'])               #Remove the file from root to avoid piling
+    data = extract_tag(tree, tag_dict["description"])   #Extract description tag data
+    print(data)                                         #Prints description data
+    print('')
+    print(get_lang(data))                               #Prints language detection output
